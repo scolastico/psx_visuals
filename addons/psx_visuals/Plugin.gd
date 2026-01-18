f337882dd6b4d0f0a5a6d5cde0b04db415b9282f
Joschua Becker <jb@scolasti.co>
2026-01-18 22:37:51 +0100

    chore: aligned plugin name & removed unnecessary comment

--- addons/psx_visuals/Plugin.gd
+++ addons/psx_visuals/Plugin.gd
@@ -1,7 +1,6 @@
 @tool extends EditorPlugin
 
-const AUTOLOAD_NAME := "PSXScreenEffects"
-# FIX: Use absolute path to ensure Godot finds the script regardless of context
+const AUTOLOAD_NAME := "PsxVisuals"
 const AUTOLOAD_PATH := "res://addons/psx_visuals/scripts/Autoload.gd"
 
 func _enable_plugin() -> void:
